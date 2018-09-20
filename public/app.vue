7e50a3a10e7a29548873a403dfc11ec1917e8ab8
Maciej Krüger <mkg20001@gmail.com>
2018-09-20 23:30:34 +0200

    feat: Vue frontend

---
+++ public/app.vue
@@ -0,0 +1,71 @@
+<template>
+  <div class="page-container">
+    <md-app md-waterfall md-mode="overlap">
+      <md-app-toolbar class="md-primary md-large">
+        <div class="md-toolbar-row">
+          <span class="md-title">YouTube Downloader</span>
+        </div>
+      </md-app-toolbar>
+
+
+      <md-app-content>
+        <center>
+          <div style="background: #00000011; border-radius: 6px; padding: 4px 16px !important; display: flex; margin: .5em 6em;">
+            <md-field>
+              <md-input class="dl-input" :disabled="isDownloadingMeta" v-model="url" placeholder="YouTube URL or other media URL"></md-input>
+            </md-field>
+            <md-progress-spinner v-if="isDownloadingMeta" :md-diameter="30" :md-stroke="3" md-mode="indeterminate"></md-progress-spinner>
+            <md-button v-else class="md-icon-button md-raised md-primary dl-btn" @click="isDownloadingMeta = true">
+              <md-icon>arrow_downward</md-icon>
+            </md-button>
+          </div>
+        </center>
+
+        <h2>Downloads</h2>
+
+        <br>
+        <br>
+        <br>
+      </md-app-content>
+    </md-app>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+  .md-app {
+    max-height: 400px;
+    border: 1px solid rgba(#000, .12);
+  }
+
+   // Demo purposes only
+  .md-drawer {
+    width: 230px;
+    max-width: calc(100vw - 125px);
+  }
+
+  .md-app-content {
+    padding: 3em;
+    margin: -64px 12em 12em !important;
+  }
+
+  .dl-btn {
+    margin: .75em;
+  }
+
+  .dl-input {
+
+  }
+
+  .md-progress-spinner {
+    margin: 24px;
+  }
+</style>
+
+<script>
+export default {
+  name: 'Overlap',
+  data: () => ({
+    isDownloadingMeta: false
+  })
+}
+</script>
---
+++ public/main.js
@@ -0,0 +1,19 @@
+'use strict'
+
+import Vue from 'vue/dist/vue.esm.js'
+import VueMaterial from 'vue-material'
+import 'vue-material/dist/vue-material.min.css'
+import 'vue-material/dist/theme/default.css'
+
+Vue.use(VueMaterial)
+
+import app from './app.vue'
+
+window.onload = () => {
+  new Vue({
+    el: '#app',
+    components: {
+      app
+    }
+  })
+}
